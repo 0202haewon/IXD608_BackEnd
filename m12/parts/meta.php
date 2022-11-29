eccb2c181891cc3ac132ce78f7ea7da819f35564
0202haewon <114978729+0202haewon@users.noreply.github.com>
2022-11-29 07:46:19 -0800

    Delete m12 directory

--- m12/parts/meta.php
+++
@@ -1,6 +0,0 @@
-<meta name="viewport" content="width=device-width">
-<base href="http://haewon.site/aau/ixd608/lee.haewon/">
-<link rel="stylesheet" href="lib/css/styleguide.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="css/storetheme.css">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
